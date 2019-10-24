46433867aa3ea53377a99046737fd60d02f4cbc6
USER25 <USER25@USER14-PC>
2019-10-24 17:00:27 +0900

    객체지향 작업 중(2)

--- BaseBallGame/src/com/hkit/bbg/oop/Game.java
+++ BaseBallGame/src/com/hkit/bbg/oop/Game.java
@@ -3,5 +3,11 @@ package com.hkit.bbg.oop;
 public class Game {
 	public void startGame(int no_cnt) {
 		RandomNumbers rnb = new RandomNumbers(no_cnt);
+		UserNumbers unb = new UserNumbers(no_cnt);
+		Result result = new Result(no_cnt);
+		
+		
+		
+		
 	}
 }
