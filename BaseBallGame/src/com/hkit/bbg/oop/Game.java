7f93b2fa77d5ac240b9aae4aa0892db22d3a98e1
USER25 <USER25@USER14-PC>
2019-10-24 17:06:39 +0900

    객체지향 작업 완료

--- BaseBallGame/src/com/hkit/bbg/oop/Game.java
+++ BaseBallGame/src/com/hkit/bbg/oop/Game.java
@@ -3,11 +3,19 @@ package com.hkit.bbg.oop;
 public class Game {
 	public void startGame(int no_cnt) {
 		RandomNumbers rnb = new RandomNumbers(no_cnt);
-		UserNumbers unb = new UserNumbers(no_cnt);
 		Result result = new Result(no_cnt);
 		
-		
-		
-		
+		for(int item : rnb.getArray()) {
+			System.out.print(item + ", ");
+		}
+		System.out.println();
+		while(true) {
+			UserNumbers unb = new UserNumbers(no_cnt);			
+			unb.setUserNumbers();			
+			if(Check.check(result, rnb, unb)) {
+				System.out.println("게임 종료!!");
+				break;
+			}
+		}
 	}
 }
